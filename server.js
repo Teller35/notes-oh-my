941a9bd8d5610a2662fa22c17e65576d23a44c2a
Teller Wetzel <tellerwetzel@yahoo.com>
2021-05-25 11:14:58 -0600

    working POST route with UUID

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-})
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public.index.html'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 app.get('/api/notes', (req, res) => {
@@ -27,6 +28,27 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json(err);
+        }
+        else {
+            const notes = JSON.parse(data);
+            req.body.id = uuidv4();
+            const addNotes = [req.body, ...notes];
+            fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(addNotes), (err) => {
+                if (err) {
+                    res.status(500).json(err);
+                }
+                else {
+                    res.json(addNotes);
+                }
+            })
+        }
+    })
+})
+
 app.listen(PORT, () => {
-    console.log(`We here you on port ${PORT}!`);
+    console.log(`We hear you on port ${PORT}!`);
 })
